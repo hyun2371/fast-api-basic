fa28a2b99fafd30d14edf603fd7f69d51233d43e
hs12 <loveanna1228@gmail.com>
2026-01-01 22:06:44 +0900

    [ch3] ORM

---
+++ src/database/connection.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+pymysql://todo_user:todos@localhost:3310/todos"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+session = SessionFactory()
+
+def get_db():
+    session = SessionFactory()
+    try:
+        yield session
+    finally:
+        session.close()
+
